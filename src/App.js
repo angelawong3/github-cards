070120eb1cab4105cce7f7e0e1a5f6883339a5e3
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-02 17:04:39 +0000

    comment out test data

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import "./App.css";
 import CardList from "./components/CardList/CardList";
 import Form from "./components/Form/Form";
 
-const testData = [
-  {
-    name: "Anya Forger",
-    avatar_url:
-      "https://www.themarysue.com/wp-content/uploads/2022/05/Anya-smile.jpg",
-    bio: "Spy x Family",
-  },
-  {
-    name: "Loid Forger",
-    avatar_url:
-      "https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/08/spy-x-family-loid-smiling.jpg",
-    bio: "Spy x Family",
-  },
-  {
-    name: "Yor Forger",
-    avatar_url:
-      "https://leosigh.com/wp-content/uploads/2022/11/Yor-in-Episode-19-rushing-to-take-Anyas-gym-clothes.jpg",
-    bio: "Spy x Family",
-  },
-];
+// const testData = [
+//   {
+//     name: "Anya Forger",
+//     avatar_url:
+//       "https://www.themarysue.com/wp-content/uploads/2022/05/Anya-smile.jpg",
+//     bio: "Spy x Family",
+//   },
+//   {
+//     name: "Loid Forger",
+//     avatar_url:
+//       "https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/08/spy-x-family-loid-smiling.jpg",
+//     bio: "Spy x Family",
+//   },
+//   {
+//     name: "Yor Forger",
+//     avatar_url:
+//       "https://leosigh.com/wp-content/uploads/2022/11/Yor-in-Episode-19-rushing-to-take-Anyas-gym-clothes.jpg",
+//     bio: "Spy x Family",
+//   },
+// ];
 
 class App extends React.Component {
   // constructor(props) {
