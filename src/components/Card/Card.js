f0d8df0e9d60ed556502a45b2659ef19c35ebd3f
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2023-01-02 16:25:34 +0000

    render multiple cards using this.props

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,9 @@
 import React from "react";
 import "./Card.css";
 
-const testData = [
-  {
-    name: "Anya Forger",
-    avatar_url:
-      "https://www.themarysue.com/wp-content/uploads/2022/05/Anya-smile.jpg",
-    company: "Spy x Family",
-  },
-  {
-    name: "Sophie Alpert",
-    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
-    company: "Humu",
-  },
-  {
-    name: "Sebastian Markbåge",
-    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
-    company: "Facebook",
-  },
-];
-
 class Card extends React.Component {
   render() {
-    const profile = testData[0];
+    const profile = this.props;
 
     return (
       <div className="github-profile">
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -2,10 +2,30 @@ import React from "react";
 import "./CardList.css";
 import Card from "../Card/Card";
 
+const testData = [
+  {
+    name: "Anya Forger",
+    avatar_url:
+      "https://www.themarysue.com/wp-content/uploads/2022/05/Anya-smile.jpg",
+    company: "Spy x Family",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+
 const CardList = (props) => {
   return (
     <div>
-      <Card />
+      <Card {...testData[0]} />
+      <Card {...testData[1]} />
     </div>
   );
 };
